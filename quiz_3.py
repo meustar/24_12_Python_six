a5be2688dc67d802b6fe16a5356209f40901d120
meustar <meustar829@gmail.com>
2025-01-03 00:05:19 +0900

    Quiz_3

--- quiz_3.py
+++ quiz_3.py
@@ -9,3 +9,11 @@ Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하
 예) 생성된 비밀번호 : nav51!
 '''
 
+# url = "http://naver.com"
+url = "http://google.com"
+my_str = url.replace("http://", "") # 규칙 1
+# print(my_str)
+my_str = my_str[:my_str.index(".")] # 규칙 2  my_str[0:5] -> 0 ~ 5 직전까지.
+# print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}의 비밀번호는 {1} 입니다.".format(url, password))
